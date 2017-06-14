f6ab4c8b9531f3d3b2ed258f9f4758a976259680
Jonathan Camerer <jon.camerer@gmail.com>
2017-06-14 19:47:14 +0000

    Added form for create menu item

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,6 +1,26 @@
 class MenuController < ApplicationController
-    
-    def index
+  
+  # Requires admin access
+  def new
+    @menu_item = Menu.new
+  end
+  
+  def create
+    @menu_item = Menu.new(menu_params)
+    if @menu_item.save
+      flash[:success] = "New Menu item saved!"
+      redirect_to new_menu_path
+    else
+      flash[:danger] = @menu_item.errors.full_messages.join(", ")
+      redirect_to new_menu_path
+    end  
+  end
+  
+  def index
+  end
+  
+  private
+    def menu_params
+      params.require(:menu).permit(:category, :name, :price, :description)
     end
-
 end
