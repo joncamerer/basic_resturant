75b5a4597e5b06b58cc83b116d581aec830be7d6
Jonathan Camerer <jon.camerer@gmail.com>
2017-06-15 00:56:02 +0000

    Set up menu page to view menu by category

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -6,17 +6,23 @@ class MenuController < ApplicationController
   end
   
   def create
-    @menu_item = Menu.new(menu_params)
-    if @menu_item.save
-      flash[:success] = "New Menu item saved!"
-      redirect_to new_menu_path
-    else
-      flash[:danger] = @menu_item.errors.full_messages.join(", ")
-      redirect_to new_menu_path
-    end  
+      @menu_item = Menu.new(menu_params)
+      
+      if @menu_item.save
+        flash[:success] = "New Menu item saved!"
+        redirect_to new_menu_path
+      else
+        flash[:danger] = @menu_item.errors.full_messages.join(", ")
+        redirect_to new_menu_path
+      end
+    
   end
   
   def index
+    if params[:category]
+      @menu_item = Menu.all
+      @display_menu = @menu_item.where(category: "#{params[:category]}" )
+    end
   end
   
   private
