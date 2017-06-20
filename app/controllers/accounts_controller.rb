6d5e20cab3119468f62c869639abbf1396635ff7
Jonathan Camerer <jon.camerer@gmail.com>
2017-06-20 20:36:20 +0000

    Only Current Member function

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,18 +3,22 @@ class AccountsController < ApplicationController
   before_action :only_current_member
   
   def new
+    @account = Account.new
   end
   
   def show
+    @account = Account.all
   end
   
   def edit
   end
 
   private
+    # Redirects user to their own account if they try to modify URL params
     def only_current_member
-      @member = Member.find( params[:member_id] )
-      redirect_to root_path unless @member == current_member
+      if "#{current_member.id}" != "#{params[:member_id]}"
+        redirect_to member_account_path(current_member.id)
+      end
     end
 
 end
