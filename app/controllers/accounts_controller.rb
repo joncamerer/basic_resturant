c21db8fd0929059ed8740c93379da25c6e680923
Jonathan Camerer <jon.camerer@gmail.com>
2017-06-20 22:41:37 +0000

    Added form to create dummy accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,13 +1,23 @@
 class AccountsController < ApplicationController
-  before_action :authenticate_member!
-  before_action :only_current_member
+  #before_action :authenticate_member!
+  #before_action :only_current_member
   
   def new
     @account = Account.new
   end
   
+  def create
+    @account = Account.new(account_params)
+    @account.save
+  end
+  
   def show
-    @account = Account.all
+    
+    # Link Member to Account based on card number
+    @all = Account.all
+    @card = current_member.card_number
+    @account = @all.where( "card_number = ?" , @card )
+    
   end
   
   def edit
@@ -19,6 +29,10 @@ class AccountsController < ApplicationController
       if "#{current_member.id}" != "#{params[:member_id]}"
         redirect_to member_account_path(current_member.id)
       end
+    end
+    
+    def account_params
+      params.require(:account).permit(:visit_count, :loyalty_points, :card_number, :join_date, :last_visit)
     end
 
 end
