6e9df3a16676787245a8f687e7b47c3f10b3a4f3
Jon Camerer <jon.camerer@gmail.com>
2017-11-29 16:00:18 -0600

    Config changes for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170614181546) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.integer  "member_id"
     t.string   "first_name"
@@ -40,8 +43,8 @@ ActiveRecord::Schema.define(version: 20170614181546) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "card_number"
-    t.index ["email"], name: "index_members_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_members_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "menus", force: :cascade do |t|
